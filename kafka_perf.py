f70b68efadc387f43a871efb5e5e4c50dcf06375
Nanfei Chen <nfchen@mathildetech.com>
2016-12-05 15:53:56 +0800

    Fixed issues

--- kafka_perf.py
+++ kafka_perf.py
@@ -10,6 +10,8 @@ KAFKA_SERVERS = os.getenv('KAFKA_SERVERS')
 THREAD_NUM = int(os.getenv('THREAD_NUM', 10))
 MAX_MESSAGE_NUM = int(os.getenv('MAX_MESSAGE_NUM', 100))
 
+thread_count = 0
+
 
 def deliver_callback(err, msg):
     nw = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -17,7 +19,9 @@ def deliver_callback(err, msg):
     if err:
         print '[%s] Failed to deliver message to kafka. Error: %s' % (nw, err)
     else:
-        print '[%s] Succeeded to deliver message to %s [%d]' % (nw, msg.topic(), msg.partition())
+        print '[%s] Succeeded to deliver message \"%s\" to %s [%d]' % (
+            nw, msg.value(), msg.topic(), msg.partition()
+        )
 
 
 def send_message(msg):
@@ -30,22 +34,36 @@ def send_message(msg):
 
 
 def multiple_run():
-    def callback():
+    start_time = datetime.now()
+
+    def callback(thread_num):
+        global thread_count
+
         cnt = 0
         while cnt < MAX_MESSAGE_NUM:
-            time.sleep(0.05)
+            time.sleep(0.01)
             cnt += 1
 
             nw = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            msg = 'A new message %d is going to be sent at %s' % (cnt, nw)
+            msg = 'Thread %d sent a new message %d at %s' % (thread_num, cnt, nw)
             send_message(msg)
+
+            print 'Message: %s' % msg
+
+        thread_count += 1
 
     count = 0
     while count < THREAD_NUM:
-        Thread(target=callback).start()
+        Thread(target=callback, args=(count,)).start()
 
         print 'Thread %d started' % count
         count += 1
+
+    while thread_count < THREAD_NUM:
+        time.sleep(1)
+
+    end_time = datetime.now()
+    print 'Running time %ds' % (end_time - start_time).total_seconds()
 
 
 multiple_run()
